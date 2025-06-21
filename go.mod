c32ed462e58f2c51ebc9d75cf9733ccd54ff9563
franciscolarrocca <francisco.larrocca@outlook.com>
2025-06-21 13:18:01 -0300

    feat: simulating database vulnerability

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module flarrocca/snyk-action-integration
 
 go 1.23.3
 
-require github.com/google/uuid v1.6.0
+require github.com/go-sql-driver/mysql v1.9.3
+
+require filippo.io/edwards25519 v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"net/http"
 
-	"github.com/google/uuid"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	query := fmt.Sprintf("SELECT * FROM users WHERE name = '%s'", name)
+	fmt.Println("Executing query: ", query)
+
+	db, err := sql.Open("mysql", "dbuser:dbpass@tcp(localhost:3306)/dbname")
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query(query)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	fmt.Fprintf(w, "Query executed successfully")
+}
+
 func main() {
-	id := uuid.New()
-	fmt.Println("Hello Snyk integration", id)
+	http.HandleFunc("/users", handler)
+	http.ListenAndServe(":8080", nil)
 }
